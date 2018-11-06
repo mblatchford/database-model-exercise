239756cd486dbd4b33310e12d1cbbc6b21b237a8
Michael Blatchford <mblatchford@mac.com>
2018-11-06 09:26:03 -0500

    Completed add, update and delete functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const list = require(`./models/groceryList`)
+
+list.addItem(`Vodka`, 2)
+    .then(result => {
+       console.log(result.rows); 
+    });
+
+list.updateItem(`3`,`blueberry bagels`, `3`)
+    .then(result => {
+        console.log(result.rows)
+    })
+
+list.deleteItem(`4`)
+    .then(result => {
+        console.log(result.rows); 
+    });
---
+++ models/groceryList.js
@@ -0,0 +1,63 @@
+const pgp = require('pg-promise')();
+
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'grocery-list-db'
+    //not used for our use locally but will be required when deployed
+    // user: 'user-name',
+    // password: 'user-password'
+});
+
+// delete item
+// add item
+// update quantity
+
+
+// CREATE
+function addItem(name, quantity){
+    return db.result(`insert into groceryitems (name, quantity) 
+        values
+            ($1,$2)
+            returning id `,
+            [name, quantity])
+        .catch(err => {
+            return {
+            name: `Unable to add item to list.`
+            };
+        })
+}
+// RETREIVE
+// how is this useful? 
+function getItem(id){
+    return db.one(`select id from groceryitems 
+        where id = $1`, [id])
+    .catch(err => {
+        return {
+        name: `Unable to retrieve item.`
+        };
+    })
+}
+// UPDATE
+function updateItem(id, name, quantity){
+    return db.result(`update groceryitems 
+        set name=$2, quantity=$3
+        where id=$1`, [id, name, quantity])
+        .catch(err => {
+            return {
+            name: `Unable to update item.`
+            };
+        })
+}
+// DELETE
+function deleteItem(id){
+    return db.result('delete from groceryitems where id = $1', [id])
+
+}
+
+module.exports = {
+    addItem,
+    getItem,
+    updateItem,
+    deleteItem
+}
